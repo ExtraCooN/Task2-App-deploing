ab4a88608de2302b6fe0aed5387fe8044e925060
ExtraCooN <156292718+ExtraCooN@users.noreply.github.com>
2024-02-21 11:34:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 session_start ();
 
 if (isset($_SESSION['login'])) {
+    $currentUrl = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
+  $newUrl = str_replace('index', 'exit', $currentUrl);
     echo 'Здравствуй, ' . $_SESSION['login'] . '!' . PHP_EOL;
-    echo ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/exit.php';
+    echo $newUrl;
+    //echo ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/exit.php';
 
 } else {
   include 'about.html';
